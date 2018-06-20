79a20e2ca286bcbe755d92d8870d8bac2e221a25
ijagberg <ijagberg@gmail.com>
2018-06-20 20:19:20 +0200

    Implemented some basic tests (which currently seem to fail)

--- RegexNfaTest/RegexNfaTest.cs
+++ RegexNfaTest/RegexNfaTest.cs
@@ -15,10 +15,18 @@ namespace RegexNfaTest
         private DeterministicFiniteAutomaton BuildTestDfa(string regex)
         {
             Parser parser = new Parser(regex);
+
             ParseTree tree = parser.Parse();
+            Debug.WriteLine($"tree: {tree}");
+
             RegexNfaConverter converter = new RegexNfaConverter();
-            RegexAutomaton automaton = converter.ConvertToNfa(tree);
-            DeterministicFiniteAutomaton dfa = RegexAutomaton.ConvertToDfa(automaton);
+
+            RegexAutomaton nfa = converter.ConvertToNfa(tree);
+            Debug.WriteLine($"nfa: {nfa}");
+
+            DeterministicFiniteAutomaton dfa = RegexAutomaton.ConvertToDfa(nfa);
+            Debug.WriteLine($"dfa: {dfa}");
+
             return dfa;
         }
 
@@ -27,19 +35,18 @@ namespace RegexNfaTest
         {
             Debug.WriteLine("Running test.");
 
-            string regex = "asd*g(d+)|(f)";
+            string regex = "asd*g(d)|(f)";
             string input = "asdddgd";
 
             DeterministicFiniteAutomaton dfa = BuildTestDfa(regex);
 
-            Debug.WriteLine($"DFA: {dfa}");
             Assert.AreEqual(dfa.Parse(input), true);
         }
 
         [TestMethod]
         public void Should_Accept_Input_String_That_Is_Part_Of_Language_2()
         {
-            string regex = "asd*g(d+)|(f)";
+            string regex = "asd*g(d)|(f)";
             string input = "asgf";
 
             DeterministicFiniteAutomaton dfa = BuildTestDfa(regex);
