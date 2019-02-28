8a1994ab26eba933acd7465d75ee53300f9a98cf
Isak Jägberg <ijagberg@gmail.com>
2019-02-28 22:10:52 +0100

    Implement basic structure of Automaton::from_tree
    I think this is how I want the method to look

--- src/automaton.rs
+++ src/automaton.rs
@@ -1,12 +1,27 @@
-use super::parse_tree;
+use super::parse_tree::ParseTree;
 
 #[derive(Debug)]
 pub struct Automaton {
-    pub id:String
+    pub id: String,
 }
 
 impl Automaton {
-    fn get_id(&self)->String {
-        self.id.clone()
+    pub fn from(parse_tree: ParseTree) {
+        Automaton::from_tree(&parse_tree);
+    }
+
+    fn from_tree(parse_tree: &ParseTree) {
+        match parse_tree {
+            ParseTree::Concatenation { left, right } => {
+                let left_dfa = Automaton::from_tree(left);
+                let right_dfa = Automaton::from_tree(right);
+            }
+            ParseTree::Or { left, right } => {}
+            ParseTree::Star { inner } => {}
+            ParseTree::Question { inner } => {}
+            ParseTree::Plus { inner } => {}
+            ParseTree::Atom(c) => {}
+            ParseTree::Empty => {}
+        }
     }
 }
