f15cb0d1235e417bc10b26d3cfe8243567ef075c
Isak Jägberg <ijagberg@gmail.com>
2019-07-24 10:26:57 +0200

    Simplify from_args example

--- examples/from_args.rs
+++ examples/from_args.rs
@@ -4,8 +4,6 @@ fn main() {
     let args: Vec<_> = std::env::args().collect();
     let regex = &args[1];
     println!("{}", regex);
-    let nfa = Automaton::from_string(&regex).unwrap();
-    let dfa = nfa.into_dfa();
-    let minimized_dfa = dfa.into_min_dfa();
+    let minimized_dfa = Automaton::from_string(&regex).unwrap().into_min_dfa();
     println!("{}", minimized_dfa.to_dot_format());
 }
