ab3bd073e7449c9228801155b37aa3bae55c221c
Isak Jägberg <ijagberg@gmail.com>
2019-07-29 23:01:43 +0200

    Remove output of regular expression

--- examples/from_args.rs
+++ examples/from_args.rs
@@ -3,7 +3,7 @@ use regex_dfa::automaton::Automaton;
 fn main() {
     let args: Vec<_> = std::env::args().collect();
     let regex = &args[1];
-    println!("{}", regex);
+    //println!("{}", regex);
     let minimized_dfa = Automaton::from_string(&regex).unwrap().into_min_dfa();
     println!("{}", minimized_dfa.to_dot_format());
 }
